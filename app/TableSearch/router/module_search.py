07f1432a5c935683076f0263ec4ca76d44bef784
kyberlox <64195832+kyberlox@users.noreply.github.com>
2026-05-25 12:26:32 +0400

    отладка поиска ошибок

--- app/TableSearch/router/module_search.py
+++ app/TableSearch/router/module_search.py
@@ -263,6 +263,7 @@ async def process_table_data(
         for col, param_name in column_to_param.items()
         if row[col]
     }
+    print(parameters)
 
     # parameters = dict()
 
@@ -271,7 +272,7 @@ async def process_table_data(
             parameters[param_name] = row[col][0]
         elif row[col] and len(row[col]) > 1:
             parameters[param_name] = sorted(str(v) for v in row[col])
-    print(parameters)
+
     # ! ???
 
     # сюда функция формульного поиска
