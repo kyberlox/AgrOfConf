5bef58fe32d902f0de5ddcf8b20a04a2f5ee80eb
kyberlox <64195832+kyberlox@users.noreply.github.com>
2026-05-25 13:25:26 +0400

    отладка поиска ошибок

--- app/TableSearch/router/module_search.py
+++ app/TableSearch/router/module_search.py
@@ -227,7 +227,7 @@ async def process_table_data(
     allowed_params = set(schema_params)
     formula_params = dict()  # добавляю формульные параметры
     for param_name, value in selected_params.items():
-        # print(param_name, value)
+        print(param_name, value)
 
         if param_name not in allowed_params:
             formula_params[param_name] = value
