0262208c5b57722dfd166293b44a46e2540ee8ef
Max-Kucher-Sar <kucherenko.max75@mail.ru>
2026-06-29 09:28:10 +0400

    Отладка выдачи ТКП

--- app/TablePakage/router/tkp_generation.py
+++ app/TablePakage/router/tkp_generation.py
@@ -48,8 +48,7 @@ async def tkp_generation(
         file_info = result.scalar_one_or_none()
         if not file_info:
             raise HTTPException(status_code=404, detail="Файл не найден")
-        template_path = template_path.file
-        print(template_path, '123')
+        template_path = file_info.file
         contact_info = ["Имя агента", "Маркировка"]
         if not all(key in user_dict for key in contact_info):
             raise HTTPException(status_code=400, detail="Не все обязательные поля заполнены")
