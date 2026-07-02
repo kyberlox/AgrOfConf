14b9c3f4022834ed96c2c3fa489b9b13f3a2b24e
Max-Kucher-Sar <kucherenko.max75@mail.ru>
2026-07-02 16:04:27 +0400

    Решаю проблему фильтра по статусу

--- app/StatisticsService/repo/elastic_statistic_repo.py
+++ app/StatisticsService/repo/elastic_statistic_repo.py
@@ -109,11 +109,12 @@ class ElasticStatisticRepo(DatabaseStatistic):
 
         if filter_keys:
             body: dict = {
-                "query": {
-                    "constant_score": {
-                        "filter": {"bool": {"filter": filter_keys}}
-                    }
-                },
+                # "query": {
+                #     "constant_score": {
+                #         "filter": {"bool": {"filter": filter_keys}}
+                #     }
+                # },
+                "query": {"bool": {"filter": filter_keys}},
                 "sort": [{"date_search": {"order": "desc"}}]
             }
         else:
