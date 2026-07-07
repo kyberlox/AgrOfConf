c50cee2f4cb7bc8570edd0fde2e197fbf08620b1
kyberlox <64195832+kyberlox@users.noreply.github.com>
2026-07-07 17:21:41 +0400

    фикс скачивания файла

--- app/main.py
+++ app/main.py
@@ -52,7 +52,7 @@ app = FastAPI(
 DOMAIN = os.getenv('DOMAIN')
 
 # # Настройка CORS
-origins = ["http://localhost:5173"]
+origins = ["http://localhost:5173", DOMAIN]
 
 app.add_middleware(
     CORSMiddleware,
