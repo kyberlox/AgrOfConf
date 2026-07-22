ff0b1eecc7190f040f0f1fd5026aae0a87689edc
Max-Kucher-Sar <kucherenko.max75@mail.ru>
2026-07-22 11:00:04 +0400

    Чертежи к продукту

--- app/TableSearch/utils/code_mode.py
+++ app/TableSearch/utils/code_mode.py
@@ -1244,7 +1244,7 @@ class CodeParametr:
         # """
         # params = {"product_id": product_id, "name": mark}
         # stmt = await db.execute(text(query), params) 
-        stmt = select(ProductDrawing.file_url).where(ProductDrawing.product_id = product_id, ProductDrawing.name = mark)
+        stmt = select(ProductDrawing.file_url).where(ProductDrawing.product_id == product_id, ProductDrawing.name == mark)
         res = await db.execute(stmt)
         request = res.scalar_one_or_none()
         if not request:
