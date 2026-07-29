336bd6fce348f3634d966e41692930e6cbbaa8ee
Max-Kucher-Sar <kucherenko.max75@mail.ru>
2026-07-29 16:32:09 +0400

    Номер ТКС в названии файла

--- app/TableSearch/router/AI.py
+++ app/TableSearch/router/AI.py
@@ -132,12 +132,12 @@ async def upload_OL(
         )
         # res = response.model_dump()
         # total_coast = res['usage']['total_cost']
-        # need = response.choices[0].message.content
-        # total_coast = response.usage.total_cost
-        # need = res['choices'][0]['message']['content']
-        # parsed_need = _extract_json_from_response(need)
+        need = response.choices[0].message.content
+        total_coast = response.usage.total_cost
+        need = res['choices'][0]['message']['content']
+        parsed_need = _extract_json_from_response(need)
         
-        # # Сохраняем статистику
+        # Сохраняем статистику
         # stat_info = await build_statistic_data(db, user_id, product_id)
         # stat_info['parameters'] = parsed_need
         # stat_info['total_coast'] = total_coast
