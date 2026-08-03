d9c36a0a936d309a08f0a451b05f1bdbcfcba11d
Max-Kucher-Sar <kucherenko.max75@mail.ru>
2026-08-03 11:39:39 +0400

    Измененные чертежи и форма ткп

--- app/TablePakage/router/tkp_generation.py
+++ app/TablePakage/router/tkp_generation.py
@@ -28,6 +28,8 @@ import requests
 from openpyxl.drawing.image import Image as XLImage
 from ..utils.kir_param_to_latin import KEY_MAPPING
 
+from PIL import Image
+import io
 
 router = APIRouter(prefix="/tkp_generation", tags=["TKP"])
 
@@ -126,8 +128,21 @@ async def tkp_generation(
                     image_bytes = f.read()
                 
                 # Создаем InlineImage из bytes
-                from io import BytesIO
-                user_dict["Чертеж"] = InlineImage(doc, BytesIO(image_bytes), width=Mm(160))
+                pil_image = Image.open(BytesIO(image_bytes))
+
+                # 2. Сбрасываем DPI в 96 (это стандарт для Word) и сохраняем в новый буфер
+                # Убираем цветовой профиль, нормализуем формат (превращаем в RGB, если был CMYK или оттенки серого)
+                if pil_image.mode != 'RGB':
+                    pil_image = pil_image.convert('RGB')
+
+                # Устанавливаем DPI в 96
+                pil_image.info['dpi'] = (96, 96)
+
+                new_buffer = BytesIO()
+                # Обязательно сохраняем в PNG или JPEG (в зависимости от того, что лучше для чертежа)
+                pil_image.save(new_buffer, format='PNG', dpi=(96, 96))
+                new_buffer.seek(0)
+                user_dict["Чертеж"] = InlineImage(doc, new_buffer, width=Mm(160))
                 # user_dict["Чертеж"] = InlineImage(doc, absolute_path, width=Mm(60)) 
             # user_dict.pop("Чертеж")
             print('***')
@@ -142,7 +157,7 @@ async def tkp_generation(
             try:
                 doc.render(new_user_dict)
             except Exception as e:
-                print(str(e))
+                print(str(e)) 
             print('***')
             result_stream = BytesIO()
             doc.save(result_stream)
