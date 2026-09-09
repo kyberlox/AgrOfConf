93caa83317f593778586ea1f60b0c09a98ac32e0
Max-Kucher-Sar <kucherenko.max75@mail.ru>
2026-09-09 12:40:40 +0400

    возвращаем ошибку назад

--- app/TablePakage/router/tkp_generation.py
+++ app/TablePakage/router/tkp_generation.py
@@ -140,7 +140,7 @@ async def tkp_generation(
             #Переводит на латиницу
             new_user_dict = dict()
             for param, value in user_dict.items():
-                if KEY_MAPPING.get(param):
+                if KEY_MAPPING.get(param) and value:
                     if isinstance(value, (int, float)):
                         value = str(value).replace('.', ',')
                     elif isinstance(value, str):
