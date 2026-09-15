996cddd0835ae878a14e963730c45276a203d9ff
Max-Kucher-Sar <kucherenko.max75@mail.ru>
2026-09-15 10:13:32 +0400

    пагинация на вывод истории подбора

--- app/StatisticsService/repo/elastic_statistic_repo.py
+++ app/StatisticsService/repo/elastic_statistic_repo.py
@@ -85,7 +85,7 @@ class ElasticStatisticRepo(DatabaseStatistic):
         date_from: Optional[str] = None,
         date_to: Optional[str] = None,
         skip: int = 0,
-        limit: Optional[int] = 10,
+        limit: int = 10,
     ) -> list:
         filter_keys = []
         if user_id:
