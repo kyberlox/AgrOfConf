b31564b098dbbcf8d04f9970410e49dc4a954f86
Fullofb3ans <igorgazinskii6@gmail.com>
2026-09-16 15:34:21 +0400

    закомментил то, что рефакторил

--- front/src/views/configurator/Configurator.vue
+++ front/src/views/configurator/Configurator.vue
@@ -361,19 +361,19 @@ export default defineComponent({
             }
         }
 
-        const handleDownloadTkp = async (variantId: number) => {
-            try {
-                const response = await Api.post(`tkp_generation/create_tkp?file_id=${variantId}&product_id=${props.id}&save_to_statistic=true`, userInputs.value, { responseType: 'blob' }, undefined, true);
-                if (response) {
-                    const contentDisposition = response.headers['content-disposition'];
-                    const filename = contentDisposition?.split('filename=')[1].replaceAll('"', '');
-                    await downloadFile(response.data, filename)
-                }
-            }
-            catch (error) {
-                toast.error(error)
-            }
-        }
+        // const handleDownloadTkp = async (variantId: number) => {
+        //     try {
+        //         const response = await Api.post(`tkp_generation/create_tkp?file_id=${variantId}&product_id=${props.id}&save_to_statistic=true`, userInputs.value, { responseType: 'blob' }, undefined, true);
+        //         if (response) {
+        //             const contentDisposition = response.headers['content-disposition'];
+        //             const filename = contentDisposition?.split('filename=')[1].replaceAll('"', '');
+        //             await downloadFile(response.data, filename)
+        //         }
+        //     }
+        //     catch (error) {
+        //         toast.error(error)
+        //     }
+        // }
 
         const sendFileToRecognition = async (fileData: FormData, fileName: string) => {
             docIsLoading.value = true;
