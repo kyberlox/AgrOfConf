7223bd0b5c7dae0addf2a890decf215e8cd496ff
B1smarQ <bgh050021@gmail.com>
2026-09-18 11:07:17 +0400

    FIX: eslint

--- front/.eslintrc.js
+++ front/.eslintrc.js
@@ -1,13 +1,8 @@
 module.exports = {
-  root: true,
-  env: {
-    node: true,
-  },
-  plugins: [@typescript - eslintt', 'import'],
-  extends: [
-    'eslint:recommended',
-    'plugin:vue/essential',
-    @vuee / typescript',
-    'plugin:prettier/recommended',
-  ],
-}
+    root: true,
+    env: {
+        node: true,
+    },
+    plugins: ["@typescript-eslint", "import"],
+    extends: ["eslint:recommended", "plugin:vue/essential", "@vue/typescript", "plugin:prettier/recommended"],
+};
