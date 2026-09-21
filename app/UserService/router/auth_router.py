5c63d1745f219d6ed8e8f8ce2bae3f397de83c71
Max-Kucher-Sar <kucherenko.max75@mail.ru>
2026-09-21 17:11:30 +0400

    Где?

--- app/UserService/router/auth_router.py
+++ app/UserService/router/auth_router.py
@@ -39,6 +39,7 @@ async def get_user(
     Если нет, создаем пользователя и возвращаем на главную.
     """
     try:
+        print(123)
         is_active = await user_info_by_session_id(session_id)
         print(123)
         if not is_active:
