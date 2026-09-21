69fa419af0484310a27a9395f94c80c69b7e9e8f
B1smarQ <bgh050021@gmail.com>
2026-09-21 09:11:25 +0400

    ADMIN FORMAT

--- front/src/views/admin/product/CertificatesModal.vue
+++ front/src/views/admin/product/CertificatesModal.vue
@@ -1,113 +1,121 @@
 <template>
-  <SlotModal v-if="isOpen" @closeModal="$emit('closeModal')">
-    <div class="flex flex-col p-[15px]">
-      <h4>Список сертификатов / документов</h4>
-      <div class="flex flex-col mt-[15px] gap-[15px] max-h-[700px] overflow-auto">
-        <div
-          class="border border-gray-200 px-[15px] py-[5px] h-[50px] rounded-[16px] bg-blue-50 flex flex-row justify-between items-center gap-[15px]"
-          v-for="file in filesList" :key="file.id">
-          <a :href="apiUrl + file.file_url.replace('/api', '')" target="_blank"
-            class="font-semibold text-blue-700 hover:text-blue-500 truncate">
-            {{ file.name }}
-          </a>
-          <div class="flex flex-row items-center gap-[12px] flex-nowrap shrink-0">
-            <span class="text-[12px] text-gray-500">Срок действия: {{ file.date_to }}</span>
-            <div class="text-[12px] underline text-red-600 cursor-pointer hover:text-red-400"
-              @click="$emit('removeFile', file.id)">
-              Удалить
+    <SlotModal v-if="isOpen" @closeModal="$emit('closeModal')">
+        <div class="flex flex-col p-[15px]">
+            <h4>Список сертификатов / документов</h4>
+            <div class="flex flex-col mt-[15px] gap-[15px] max-h-[700px] overflow-auto">
+                <div
+                    class="border border-gray-200 px-[15px] py-[5px] h-[50px] rounded-[16px] bg-blue-50 flex flex-row justify-between items-center gap-[15px]"
+                    v-for="file in filesList"
+                    :key="file.id">
+                    <a
+                        :href="apiUrl + file.file_url.replace('/api', '')"
+                        target="_blank"
+                        class="font-semibold text-blue-700 hover:text-blue-500 truncate">
+                        {{ file.name }}
+                    </a>
+                    <div class="flex flex-row items-center gap-[12px] flex-nowrap shrink-0">
+                        <span class="text-[12px] text-gray-500">Срок действия: {{ file.date_to }}</span>
+                        <div
+                            class="text-[12px] underline text-red-600 cursor-pointer hover:text-red-400"
+                            @click="$emit('removeFile', file.id)">
+                            Удалить
+                        </div>
+                    </div>
+                </div>
             </div>
-          </div>
+            <div
+                class="mt-[15px] flex flex-col gap-[15px] border border-gray-200 rounded-[16px] p-[20px] max-w-[500px]">
+                <span>Для добавления нового документа - заполните название, срок действия и выберите файл</span>
+                <BaseInput
+                    v-for="field in certificateFields"
+                    :key="field"
+                    :inputSettings="initInputProps(field)"
+                    @value-changed="(value) => updateField(field, value)" />
+                <UploadFileArea
+                    :disabled="!fileName || !fileDateTo"
+                    :formats="'.pdf,.jpg,.jpeg,.png,.docx,.xlsx'"
+                    @ready-to-upload-file="uploadCertToProduct">
+                    Добавить
+                </UploadFileArea>
+            </div>
         </div>
-      </div>
-      <div class="mt-[15px] flex flex-col gap-[15px] border border-gray-200 rounded-[16px] p-[20px] max-w-[500px]">
-        <span>Для добавления нового документа - заполните название, срок действия и выберите
-          файл</span>
-        <BaseInput v-for="field in certificateFields" :key="field" :inputSettings="initInputProps(field)"
-          @value-changed="(value) => updateField(field, value)" />
-        <UploadFileArea :disabled="!fileName || !fileDateTo" :formats="'.pdf,.jpg,.jpeg,.png,.docx,.xlsx'"
-          @ready-to-upload-file="uploadCertToProduct">
-          Добавить
-        </UploadFileArea>
-      </div>
-    </div>
-  </SlotModal>
+    </SlotModal>
 </template>
 
 <script lang="ts">
-import { defineComponent, ref, type PropType } from 'vue'
-import UploadFileArea from '@/components/layout/UploadFileArea.vue'
-import { type IProductFile } from '@/assets/interfaces/IProductFile.ts'
-import { BaseInput } from 'beans-ui-kit'
-import SlotModal from '@/components/layout/SlotModal.vue'
+import { defineComponent, ref, type PropType } from "vue";
+import UploadFileArea from "@/components/layout/UploadFileArea.vue";
+import { type IProductFile } from "@/assets/interfaces/IProductFile.ts";
+import { BaseInput } from "beans-ui-kit";
+import SlotModal from "@/components/layout/SlotModal.vue";
 
-const certificateFields = ['name', 'date_to'] as const
+const certificateFields = ["name", "date_to"] as const;
 
 export default defineComponent({
-  components: {
-    UploadFileArea,
-    BaseInput,
-    SlotModal,
-  },
-  props: {
-    filesList: {
-      type: Array as PropType<IProductFile[]>,
+    components: {
+        UploadFileArea,
+        BaseInput,
+        SlotModal,
     },
-    id: {
-      type: String,
-      required: true,
+    props: {
+        filesList: {
+            type: Array as PropType<IProductFile[]>,
+        },
+        id: {
+            type: String,
+            required: true,
+        },
+        isLoading: {
+            type: Boolean,
+            default: false,
+        },
+        isOpen: {
+            type: Boolean,
+            default: false,
+        },
     },
-    isLoading: {
-      type: Boolean,
-      default: false,
+    emits: ["updateFilesList", "removeFile", "closeModal"],
+    setup(props, { emit }) {
+        const fileName = ref<string>();
+        const fileDateTo = ref<string>();
+        const apiUrl = import.meta.env.VITE_API_URL;
+
+        const uploadCertToProduct = (formDataFile: FormData) => {
+            const file = formDataFile.get("file");
+            if (!(file instanceof Blob) || !fileName.value || !fileDateTo.value) return;
+            const uploadData = new FormData();
+            uploadData.append("image", file);
+            uploadData.append("product_id", props.id);
+            uploadData.append("name", fileName.value);
+            uploadData.append("date_to", fileDateTo.value);
+            emit("updateFilesList", uploadData);
+            fileName.value = "";
+            fileDateTo.value = "";
+        };
+
+        const initInputProps = (field: "name" | "date_to") => {
+            return {
+                class: "input-admin",
+                placeholder: field == "date_to" ? "Введите срок действия (ДД.ММ.ГГГГ)" : "Введите название документа",
+                disabled: props.isLoading,
+                value: field == "date_to" ? fileDateTo.value : fileName.value,
+            };
+        };
+
+        const updateField = (field: "name" | "date_to", value: string) => {
+            if (field === "name") fileName.value = value;
+            else fileDateTo.value = value;
+        };
+
+        return {
+            apiUrl,
+            fileName,
+            fileDateTo,
+            certificateFields,
+            initInputProps,
+            updateField,
+            uploadCertToProduct,
+        };
     },
-    isOpen: {
-      type: Boolean,
-      default: false,
-    },
-  },
-  emits: ['updateFilesList', 'removeFile', 'closeModal'],
-  setup(props, { emit }) {
-    const fileName = ref<string>()
-    const fileDateTo = ref<string>()
-    const apiUrl = import.meta.env.VITE_API_URL
-
-    const uploadCertToProduct = (formDataFile: FormData) => {
-      const file = formDataFile.get('file')
-      if (!(file instanceof Blob) || !fileName.value || !fileDateTo.value) return
-      const uploadData = new FormData()
-      uploadData.append('image', file)
-      uploadData.append('product_id', props.id)
-      uploadData.append('name', fileName.value)
-      uploadData.append('date_to', fileDateTo.value)
-      emit('updateFilesList', uploadData)
-      fileName.value = ''
-      fileDateTo.value = ''
-    }
-
-    const initInputProps = (field: 'name' | 'date_to') => {
-      return {
-        class: 'input-admin',
-        placeholder:
-          field == 'date_to' ? 'Введите срок действия (ДД.ММ.ГГГГ)' : 'Введите название документа',
-        disabled: props.isLoading,
-        value: field == 'date_to' ? fileDateTo.value : fileName.value,
-      }
-    }
-
-    const updateField = (field: 'name' | 'date_to', value: string) => {
-      if (field === 'name') fileName.value = value
-      else fileDateTo.value = value
-    }
-
-    return {
-      apiUrl,
-      fileName,
-      fileDateTo,
-      certificateFields,
-      initInputProps,
-      updateField,
-      uploadCertToProduct,
-    }
-  },
-})
+});
 </script>
--- front/src/views/admin/product/components/PromptEditBlock.vue
+++ front/src/views/admin/product/components/PromptEditBlock.vue
@@ -1,71 +1,74 @@
 <template>
-<div class="flex flex-col gap-2 mt-4 border border-gray-200 p-[20px] rounded-xl bg-gray-50"
-     v-if="Object.keys(systemPrompt).length">
-    <h3 class="text-lg font-medium mb-2">Системный промпт</h3>
-    <div v-for="item in ['validation_prompt', 'unified_prompt']"
-         :key="item">
-        <span>{{ ruPromptNames[item as keyof typeof ruPromptNames] }}</span>
-        <BaseTextarea class="mt-[10px]"
-                      :textarea-settings="initTextareaProps(item, String(systemPrompt[item as keyof typeof systemPrompt]) || '')"
-                      @value-changed="(val) => systemPrompt[item as keyof typeof systemPrompt] = val" />
-    </div>
-    <span>Значения по умолчанию</span>
-    <div v-if="systemPrompt.rules_table?.length"
-         class="bg-white">
-        <div class="grid grid-cols-4 gap-[20px]  p-[15px]">
-            <div v-for="(rule, index) in systemPrompt.rules_table"
-                 :key="'rule' + index">
-                <div class="flex flex-row item-center justify-center content-center gap-[5px]">
-                    <BaseInput :inputSettings="initInputSettings(rule, index)"
-                               @valueChanged="(value: string) => handleValueChanged(rule.name, value)" />
-                    <div class="text-sm mb-auto bg-red-500 rounded-md w-[25px] text-center  text-white hover:bg-red-600 cursor-pointer"
-                         @click="deleteRule(rule.name)">
-                        X</div>
+    <div
+        class="flex flex-col gap-2 mt-4 border border-gray-200 p-[20px] rounded-xl bg-gray-50"
+        v-if="Object.keys(systemPrompt).length">
+        <h3 class="text-lg font-medium mb-2">Системный промпт</h3>
+        <div v-for="item in ['validation_prompt', 'unified_prompt']" :key="item">
+            <span>{{ ruPromptNames[item as keyof typeof ruPromptNames] }}</span>
+            <BaseTextarea
+                class="mt-[10px]"
+                :textarea-settings="
+                    initTextareaProps(item, String(systemPrompt[item as keyof typeof systemPrompt]) || '')
+                "
+                @value-changed="(val) => (systemPrompt[item as keyof typeof systemPrompt] = val)" />
+        </div>
+        <span>Значения по умолчанию</span>
+        <div v-if="systemPrompt.rules_table?.length" class="bg-white">
+            <div class="grid grid-cols-4 gap-[20px] p-[15px]">
+                <div v-for="(rule, index) in systemPrompt.rules_table" :key="'rule' + index">
+                    <div class="flex flex-row item-center justify-center content-center gap-[5px]">
+                        <BaseInput
+                            :inputSettings="initInputSettings(rule, index)"
+                            @valueChanged="(value: string) => handleValueChanged(rule.name, value)" />
+                        <div
+                            class="text-sm mb-auto bg-red-500 rounded-md w-[25px] text-center text-white hover:bg-red-600 cursor-pointer"
+                            @click="deleteRule(rule.name)">
+                            X
+                        </div>
+                    </div>
+                </div>
+                <div class="py-[20px]">
+                    <BaseButton :button-settings="{ class: 'button-secondary' }" @clicked="showParamsModal = true">
+                        Добавить
+                    </BaseButton>
                 </div>
             </div>
-            <div class="py-[20px]">
-                <BaseButton :button-settings="{ class: 'button-secondary' }"
-                            @clicked="showParamsModal = true">
-                    Добавить
-                </BaseButton>
-            </div>
         </div>
-    </div>
-    <BaseButton class="mt-[12px] flex flex-row items-center max-w-fit"
-                :buttonSettings="{ class: 'button-primary', disabled: promptIsLoading }"
-                @clicked="saveNewPrompt">
-        <span v-if="!promptIsLoading">Подтвердить</span>
-        <Loader class="max-w-fit"
-                v-else />
-    </BaseButton>
+        <BaseButton
+            class="mt-[12px] flex flex-row items-center max-w-fit"
+            :buttonSettings="{ class: 'button-primary', disabled: promptIsLoading }"
+            @clicked="saveNewPrompt">
+            <span v-if="!promptIsLoading">Подтвердить</span>
+            <Loader class="max-w-fit" v-else />
+        </BaseButton>
 
-    <SlotModal v-if="showParamsModal"
-               @closeModal="showParamsModal = false">
-        <div class="max-h-[500px] overflow-y-auto flex flex-col gap-[10px]">
-            <div v-for="(item, index) in params"
-                 :key="'promptParam' + index"
-                 class="border border-b border-gray-300 p-[5px] rounded-[10px]"
-                 @click="setRule(item.name)">
-                {{ item.name }}
+        <SlotModal v-if="showParamsModal" @closeModal="showParamsModal = false">
+            <div class="max-h-[500px] overflow-y-auto flex flex-col gap-[10px]">
+                <div
+                    v-for="(item, index) in params"
+                    :key="'promptParam' + index"
+                    class="border border-b border-gray-300 p-[5px] rounded-[10px]"
+                    @click="setRule(item.name)">
+                    {{ item.name }}
+                </div>
             </div>
-        </div>
-    </SlotModal>
-</div>
+        </SlotModal>
+    </div>
 </template>
 
-<script lang='ts'>
-import { defineComponent, onMounted, ref, type PropType } from 'vue';
-import { BaseButton, BaseTextarea, BaseInput } from 'beans-ui-kit';
-import Api from '@/utils/Api';
-import SlotModal from '@/components/layout/SlotModal.vue';
-import { type IParameter } from '@/assets/interfaces/IParameter';
-import Loader from '@/components/layout/Loader.vue';
+<script lang="ts">
+import { defineComponent, onMounted, ref, type PropType } from "vue";
+import { BaseButton, BaseTextarea, BaseInput } from "beans-ui-kit";
+import Api from "@/utils/Api";
+import SlotModal from "@/components/layout/SlotModal.vue";
+import { type IParameter } from "@/assets/interfaces/IParameter";
+import Loader from "@/components/layout/Loader.vue";
 
 interface ISystemPrompt {
-    product_id?: number,
-    validation_prompt?: string,
-    unified_prompt?: string,
-    rules_table?: { name: string, default: string }[]
+    product_id?: number;
+    validation_prompt?: string;
+    unified_prompt?: string;
+    rules_table?: { name: string; default: string }[];
 }
 
 export default defineComponent({
@@ -75,70 +78,67 @@ export default defineComponent({
             type: Number,
         },
         params: {
-            type: Array<IParameter>
-        }
+            type: Array<IParameter>,
+        },
     },
     setup(props) {
-
         const systemPrompt = ref<ISystemPrompt>({});
         const promptIsLoading = ref(false);
         const showParamsModal = ref(false);
 
         const ruPromptNames = {
-            validation_prompt: 'Промпт первого слоя',
-            unified_prompt: 'Промпт второго слоя'
-        }
+            validation_prompt: "Промпт первого слоя",
+            unified_prompt: "Промпт второго слоя",
+        };
 
         onMounted(async () => {
-            systemPrompt.value = await Api.get(`/AI/get_product_prompt/${props.id}`)
-        })
+            systemPrompt.value = await Api.get(`/AI/get_product_prompt/${props.id}`);
+        });
 
         const initTextareaProps = (name: string, value: string) => {
             return {
-                class: 'textarea-admin',
+                class: "textarea-admin",
                 value: value,
                 name: name,
-                placeholder: 'Введите системный промпт',
-                disabled: false
-            }
-        }
+                placeholder: "Введите системный промпт",
+                disabled: false,
+            };
+        };
 
-        const initInputSettings = (param: { name: string, default: string }, index: number | string) => {
+        const initInputSettings = (param: { name: string; default: string }, index: number | string) => {
             return {
-                class: 'input-param--no-group',
-                placeholder: 'Впишите значение',
+                class: "input-param--no-group",
+                placeholder: "Впишите значение",
                 value: param.default,
                 name: param.name + (Number(index) + 1),
                 label: param.name,
-                disabled: promptIsLoading.value
-            }
-        }
+                disabled: promptIsLoading.value,
+            };
+        };
 
         const saveNewPrompt = async () => {
             try {
                 promptIsLoading.value = true;
-                const data = await Api.post(`/AI/save_product_prompt/${props.id}`, systemPrompt.value)
+                const data = await Api.post(`/AI/save_product_prompt/${props.id}`, systemPrompt.value);
             } finally {
                 promptIsLoading.value = false;
             }
-        }
+        };
 
         const setRule = (item: string) => {
-            systemPrompt.value.rules_table?.push({ name: item, default: '' })
-            showParamsModal.value = false
-        }
+            systemPrompt.value.rules_table?.push({ name: item, default: "" });
+            showParamsModal.value = false;
+        };
 
         const deleteRule = (item: string) => {
-            systemPrompt.value.rules_table = systemPrompt.value.rules_table?.filter(e => e.name !== item)
-        }
+            systemPrompt.value.rules_table = systemPrompt.value.rules_table?.filter((e) => e.name !== item);
+        };
 
         const handleValueChanged = (name: string, value: string) => {
-            if (value == undefined) return
-            const target = systemPrompt.value.rules_table?.find(e => e.name == name);
-            if (target)
-                target.default = value
-        }
-
+            if (value == undefined) return;
+            const target = systemPrompt.value.rules_table?.find((e) => e.name == name);
+            if (target) target.default = value;
+        };
 
         return {
             systemPrompt,
@@ -150,9 +150,9 @@ export default defineComponent({
             handleValueChanged,
             saveNewPrompt,
             initTextareaProps,
-            initInputSettings
-        }
-    }
+            initInputSettings,
+        };
+    },
 });
 </script>
 
