765a6cd6f1031412b502db5a7b1c08b79989f92a
Max-Kucher-Sar <kucherenko.max75@mail.ru>
2026-09-21 16:59:07 +0400

    с прода на прод

--- app/UserService/utils/auth_utils.py
+++ app/UserService/utils/auth_utils.py
@@ -67,7 +67,7 @@ async def delete_session(session_id: str, key: Optional[str] = None) -> None:
     redis_storage.delete_session(session_id)
 
 async def refresh_session_id(token: str):
-    url = "http://intranet.emk.org.ru/api/auth_router/refresh"
+    url = "https://intranet.emk.ru/api/auth_router/refresh"
     async with httpx.AsyncClient(timeout=30.0) as client:
         res = await client.get(url, cookies={'session_id': token})
         if res.status_code == 200:
